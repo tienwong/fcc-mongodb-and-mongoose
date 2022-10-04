5faabc8e82795fdaf7f83ccb1ec3327a07414882
Tien <insanebaoposse@protonmail.com>
2022-10-03 21:37:22 -0500

    Added logic to connect to MongoDB

--- myApp.js
+++ myApp.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
+const mongoose = require('mongoose')
 
+try {
+  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  console.log('MongoDB is connected...')
+}
+catch (err) {
+  console.log(err)
+}
 
 let Person;
 
